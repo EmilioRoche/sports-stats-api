8bd306f24ee16808b27eb69059e6d9821ed2248e
EmilioRoche <32501779+EmilioRoche@users.noreply.github.com>
2022-08-29 18:02:30 -0600

    added engine in package.json

--- routes/sports.js
+++ routes/sports.js
@@ -10,7 +10,7 @@ router.get('/', async function (req, res) {
 		method: 'GET',
 		headers: {
 			'X-RapidAPI-Host': 'sportscore1.p.rapidapi.com',
-			'X-RapidAPI-Key': ENV['API_KEY']
+			'X-RapidAPI-Key': process.env.API_KEY
 		}
 	};
 	// fetches the url and gets the response as a json
