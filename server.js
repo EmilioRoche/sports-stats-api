ca3f23329e91e3f4339b9986c29d1b475482e6de
EmilioRoche <32501779+EmilioRoche@users.noreply.github.com>
2022-08-29 18:08:30 -0600

    switched route path for sports json data

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const sports = require('./routes/sports');
 
 app.use(express.json());
 
-app.use('/sports', sports);
+//we set it as default blank since we want it to load the sports list right away
+app.use('/', sports);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening on Port: ${port}`));
