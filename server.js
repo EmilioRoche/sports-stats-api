e1670ff662a8e76a2773030ab27bf97e6738088a
EmilioRoche <32501779+EmilioRoche@users.noreply.github.com>
2022-09-13 23:10:20 -0600

    added route for Football prem standings

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express();
 const sports = require('./routes/sports');
+const premstandings = require('./routes/premstandings');
 
 app.use(express.json());
 
 //we set it as default blank since we want it to load the sports list right away
 app.use('/', sports);
+app.use('/Football', premstandings);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening on Port: ${port}`));
