832941af4a68b0238d93107d00749bbc448f7e45
EmilioRoche <32501779+EmilioRoche@users.noreply.github.com>
2022-09-20 13:45:37 -0600

    aded routes for the leagues, but need to clean up each route since it has repetitive code which i'll put into a method/function

---
+++ routes/cplstandings.js
@@ -0,0 +1,34 @@
+const { response } = require('express');
+const express = require('express');
+const fetch = require('node-fetch');
+const router = express.Router();
+
+const url1 = 'https://sportscore1.p.rapidapi.com/seasons/13379/standings-tables';
+
+
+//get it to run on the football page when that url has been hit
+router.get('/', async function (req, res) {
+	res.header("Access-Control-Allow-Origin", "*");
+	const url = url1;
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Host': 'sportscore1.p.rapidapi.com',
+			'X-RapidAPI-Key': process.env.API_KEY_2
+		}
+	};
+	// fetches the url and gets the response as a json
+	fetch(url, options)
+		.then(res => res.json())
+		.then(json => console.log(json))
+		.catch(err => console.error('error:' + err));
+	try {
+		let response = await fetch(url, options);
+		response = await response.json();
+		res.status(200).json(response);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({msg: `Internal Server Error.`});
+	}
+});
+module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require('express')
 const app = express();
 const sports = require('./routes/sports');
 const premstandings = require('./routes/premstandings');
+const bundesstandings = require('./routes/bundesstandings');
+const ligastandings = require('./routes/ligastandings');
+const liguestandings = require('./routes/liguestandings');
+const seriestandings = require('./routes/seriestandings');
+const cplstandings = require('./routes/cplstandings');
 
 app.use(express.json());
 
 //we set it as default blank since we want it to load the sports list right away
 app.use('/', sports);
 app.use('/Football/Prem', premstandings);
+app.use('/Football/LaLiga', ligastandings);
+app.use('/Football/Bundesliga', bundesstandings);
+app.use('/Football/Ligue1', liguestandings);
+app.use('/Football/CPL', cplstandings);
+app.use('/Football/SerieA', seriestandings);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening on Port: ${port}`));
