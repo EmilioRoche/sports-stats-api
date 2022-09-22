644be855de18d0e448d734e9b39e6a8a1d5c5635
EmilioRoche <32501779+EmilioRoche@users.noreply.github.com>
2022-09-21 22:15:54 -0600

    fixed database call?

--- db.js
+++ db.js
@@ -1,23 +1,12 @@
-const mongoClient = require('mongodb').MongoClient;
-const mongoDbUrl = process.env.MONGODB_URI;
-let mongodb;
+const { MongoClient } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
 
-function connect(callback){
-    mongoClient.connect(mongoDbUrl, (err, db) => {
-        mongodb = db;
-        callback();
-    });
-}
-function get(){
-    return mongodb;
-}
-
-function close(){
-    mongodb.close();
-}
-
-module.exports = {
-    connect,
-    get,
-    close
-};
+(async () => {
+  try {
+    await client.connect();
+    await client.close();
+  } catch (e) {
+    console.log('Error: ', e.message);
+  }
+})();
