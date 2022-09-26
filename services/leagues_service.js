6c7d8f8415184a50d430520098fcd10bd6479670
EmilioRoche <32501779+EmilioRoche@users.noreply.github.com>
2022-09-25 20:08:09 -0600

    change the file into cleaner code

--- services/leagues_service.js
+++ services/leagues_service.js
@@ -4,9 +4,9 @@ const router = express.Router();
 
 db.connect();
 router.get('/', async function(req, res) { 
-
     try {
         db.connect();
+        //put the db query into a service and this file into routes
         let response = await db.query(`SELECT * FROM football_leagues;`);
         db.end();
         res.status(200).json(response.rows);
