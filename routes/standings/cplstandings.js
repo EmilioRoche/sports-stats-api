520758d830c6a0d8a70295141762fc917ba62652
EmilioRoche <32501779+EmilioRoche@users.noreply.github.com>
2022-09-30 17:29:47 -0600

    added configuration for teams data to be displayed

--- routes/standings/cplstandings.js
+++ routes/standings/cplstandings.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../services/db');
+const db = require('../../services/db');
 
 router.get('/', async function(req, res) { 
     try {
         //put the db query into a service and this file into routes
-        let response = await db.query(`SELECT * FROM serie_standings ORDER BY position ASC;`);
+        let response = await db.query(`SELECT * FROM cpl_standings ORDER BY position ASC;`);
         res.status(200).json(response.rows);
     } catch (err) {
         console.log(err);
--- server.js
+++ server.js
@@ -2,21 +2,22 @@ const express = require('express')
 const app = express();
 //const sports = require('./routes/sports');
 const db = require('./services/db');
-const premstandings = require('./routes/premstandings');
-const bundesstandings = require('./routes/bundesstandings');
-const ligastandings = require('./routes/ligastandings');
-const liguestandings = require('./routes/liguestandings');
-const seriestandings = require('./routes/seriestandings');
-const cplstandings = require('./routes/cplstandings');
+const premstandings = require('./routes/standings/premstandings');
+const bundesstandings = require('./routes/standings/bundesstandings');
+const ligastandings = require('./routes/standings/ligastandings');
+const liguestandings = require('./routes/standings/liguestandings');
+const seriestandings = require('./routes/standings/seriestandings');
+const cplstandings = require('./routes/standings/cplstandings');
+const prem = require('./routes/teams/prem');
 const leagues = require('./routes/leagues');
 const cors = require('cors');
 
 db.connect();
 app.use(cors());
 app.use(express.json());
-//we set it as default blank since we want it to load the sports list right away
-// will use this later on
+/*we set it as default blank since we want it to load the sports list right away and will use this later on*/
 //app.use('/',sports);
+//standings
 app.use('/Football/Prem', premstandings);
 app.use('/Football/LaLiga', ligastandings);
 app.use('/Football/Bundesliga', bundesstandings);
@@ -26,6 +27,7 @@ app.use('/Football/SerieA', seriestandings);
 
 app.use('/Football/Leagues', leagues);
 
+app.use('/Football/Prem/Teams', prem);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
