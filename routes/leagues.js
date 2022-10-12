cd12486b0fa48a120afaaee4005c23a4cb002722
EmilioRoche <32501779+EmilioRoche@users.noreply.github.com>
2022-10-12 14:33:18 -0600

    added controller for bundesliga, need to do it for remaining leagues

--- routes/leagues.js
+++ routes/leagues.js
@@ -2,6 +2,5 @@ const express = require('express');
 const router = express.Router();
 const leaguesController = require('../controllers/leagues');
 
-//db.connect();
 router.get('/', leaguesController.leagues);
 module.exports = router;
